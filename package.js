e21ddcd8088ea7e593b0029aa141265dfd36f535
Roman <rgnevashev@gmail.com>
2016-03-23 17:09:40 +0300

    Update package.js

--- package.js
+++ package.js
@@ -2,7 +2,7 @@ Package.describe({
   name: 'onokumus:metismenu',
   version: '2.4.2',
   summary: 'A jQuery menu plugin',
-  git: 'https://github.com/rgnevashev/metisMenu',
+  git: 'https://github.com/rgnevashev/metisMenu.git',
   documentation: 'README.md'
 });
 
